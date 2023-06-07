e50f8127d09fc88769b35b3cb5fe5b3108fa53d2
sam eze <meta.prof@yahoo.com>
2023-06-06 22:28:46 -0500

    first change

--- simple-webapp1/app.py
+++ simple-webapp1/app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return "Welcome to Docker Class at JJTECH!"
 
-@app.route('/how are you')
-def hello():
-    return 'I sure do good prof. How about you?'
+# @app.route('/how are you')
+# def hello():
+#     return 'I sure do good prof. How about you?'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
